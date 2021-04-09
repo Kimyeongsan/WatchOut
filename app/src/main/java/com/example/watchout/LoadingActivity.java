b5a7166ee360f909473ae3f21492b5650e0c3cf9
Kimyeongsan <apua34@gmail.com>
2021-04-09 23:03:20 +0900

    Loading 화면, Main 화면 추가
    이미지 추가 및 Main 화면 버튼 분할 완료

---
+++ app/src/main/java/com/example/watchout/LoadingActivity.java
@@ -0,0 +1,24 @@
+package com.example.watchout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LoadingActivity.this,MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
